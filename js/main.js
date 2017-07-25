2baff493b1534b7a838af8e8dd07cb389e4e6bc1
Jasmine Quach <xjasminequach@gmail.com>
2017-07-25 15:42:20 -0500

    Adding js markup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 console.log("main.js active");
 
+let japButton = document.getElementById('getJapLang');
+japButton.addEventListener('click', getJapLang);
+
+function getJapLang(event) {
+  Lang.getJap();
+}
 /***********  SCRIPT  ***************/
 
 
